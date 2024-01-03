32d6bcf8587eb109e4c70bcb48698d14c2c50f88
saviusz <saviusz@users.noreply.github.com>
2024-01-03 14:13:43 +0000

    deploy: 3973e8d8667b6d0c4c790fd384204ad4fe8b4ca6

--- _nuxt/static/1642438734/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSaviPrograms\u002F_nuxt\u002Fstatic\u002F1642438734",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FSaviPrograms\u002F",assetsPath:"\u002FSaviPrograms\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ae7322b2"}}}}(null));
